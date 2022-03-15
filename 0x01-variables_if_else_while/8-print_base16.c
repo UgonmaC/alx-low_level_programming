80dfd999403cc514d796e08a8d1e13f11ea311bf
UgonmaC <magdy.clems@gmail.com>
2022-03-14 17:44:30 -0700

    print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char z;
+for (z = '0'; z <= '9'; z++)
+{
+	putchar(z);
+}
+for (z = 'a'; z <= 'f'; z++)
+{
+	putchar(z)
+}
+	putchar('\n');
+	return (0);
+}
